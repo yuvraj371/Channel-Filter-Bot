9453001bcc6918e7084546a790307b00991d9e43
yuvraj371 <139117085+yuvraj371@users.noreply.github.com>
2023-08-21 13:52:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
-SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+API_ID       = 21938068
+API_HASH     = "c18fd98f3e58484df0aecd95a3d5a6a9"
+BOT_TOKEN    = "6562955607:AAGj9wMM5oC6uoZ6FdenHjd7k5lbnbsgXhk"
+SESSION      = "BQFOv5QAxxYgV9tphIya1AnculhwWeZze5GbeezhWWJHEKKMzt5QqpsIpEx4W63T1f8FTWyyShYhs1ftjWa0TEmhJQSW95iS5nW10vpPg51kMW7ByurEhIs9DiISv1Odnp2Mi23SguTywza621vNjqPZ32U2VOUPYuHqwOQj1m9FHHkR9E0zOmJzShyEs7qVhd0eRot7k8ZeNQdxsx-hcnBnmKZ92VlwU53wNpVy4ZFP2c-uFuM10oBIbSFsYeJpnR8nY5VeYQi0fwNUAykT_2Ov5CQKJFmIxVzXKp1PFLf_mIHJ95v9-__SlFX7r4KFgZJsVsukpoY6XJlFjg9WmeY7LGQ7eQAAAAFY9mSPAA"
+DATABASE_URI = "mongodb+srv://Postlinksearchbot:yuvraj178@postlinksearchbot.px7hujq.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001865528393
+ADMIN        = 5787509903
+CHANNEL      = "@Infinity_XBotz"
